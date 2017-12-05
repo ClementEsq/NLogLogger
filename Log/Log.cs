dfd037bde3b679b1007d3190af430e0fb208bd02
Clement Oniovosa <clement.o@outlook.com>
2017-12-05 21:51:38 +0000

    Initial source code commit

---
+++ Log/Log.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    public static class Log
+    {
+        private static Logger objInstance { get; set; }
+
+        public static void LogMessage(Logger objLogger, LogLevel logLevel, string strMessage, string strFunctionName, string strParameters)
+        {
+            objInstance = objLogger;
+            objInstance.Log(logLevel, "Message: {0}. Function: {1}. Parameters: {2}.", strMessage, strFunctionName, strParameters);
+        }
+    }
+}
